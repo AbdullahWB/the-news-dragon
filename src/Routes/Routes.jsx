d064233afd384efa6164b96c2fa60161b483c5a9
AbdullahWB <abdullah917828@gmail.com>
2023-04-22 02:17:22 +0600

    show data and show cart

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home/Home";
+import Category from "../Pages/Home/Category/Category";
+import NewsLayout from "../Layouts/NewsLayout";
+import News from "../Pages/News/News/News";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +13,22 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/category/:id',
+                element: <Category></Category>,
+                loader: ({params})=> fetch(`http://localhost:3000/categories/${params.id}`)
+            },
+        ]
+    },
+    {
+        path: '/news',
+        element: <NewsLayout></NewsLayout>,
+        children: [
+            {
+                path: ':id',
+                element: <News></News>,
+                loader: ({params})=> fetch(`http://localhost:3000/news/${params.id}`)
             }
         ]
     }
