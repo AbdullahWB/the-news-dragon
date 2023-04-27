3f789c115dd23763cbe6951a381705a0a5a58f0e
AbdullahWB <abdullah917828@gmail.com>
2023-04-27 20:45:36 +0600

    editor news cart style change

--- src/Pages/News/EditorsInside/EditorsInside.jsx
+++ src/Pages/News/EditorsInside/EditorsInside.jsx
@@ -5,48 +5,48 @@ import Row from 'react-bootstrap/Row';
 import first from '../../../assets/editorsInsight1.png'
 import second from '../../../assets/editorsInsight2.png'
 import third from '../../../assets/editorsInsight3.png'
+import moment from 'moment';
+import { FaCalendarAlt } from "react-icons/fa";
 
 const EditorsInside = () => {
     return (
-        <div>
+        <div className='container p-0'>
+            <h3 className='mb-3'>Editors Insight</h3>
             <Row xs={1} md={2} lg={3} className="g-4">
                 <Col>
-                    <Card>
-                        <Card.Img variant="top" src={first} />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                    <Card className='p-4 w-100 h-100'>
+                        <Card.Img className='image-fluid h-100' variant="top" src='https://i.pinimg.com/550x/08/91/87/089187cf1bab83ce8633e4deb6c1ab01.jpg' />
+                        <Card.Body className='px-0'>
+                            <Card.Title>Fashion News</Card.Title>
                             <Card.Text>
-                                This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit
-                                longer.
+                            21 The Most Stylish Wedding Guest Dresses For Spring
                             </Card.Text>
                         </Card.Body>
+                        <p><FaCalendarAlt></FaCalendarAlt> {moment().add(10, 'days').calendar()}</p>
                     </Card>
                 </Col>
                 <Col>
-                    <Card>
-                        <Card.Img variant="top" src={second} />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                    <Card className='p-4 w-100 h-100'>
+                        <Card.Img className='image-fluid h-100' variant="top" src='https://editorial01.shutterstock.com/preview-440/10181138b/7614b35b/Shutterstock_10181138b.jpg' />
+                        <Card.Body className='px-0'>
+                            <Card.Title>Fashion News</Card.Title>
                             <Card.Text>
-                                This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit
-                                longer.
+                            21 The Most Stylish Wedding Guest Dresses For Spring
                             </Card.Text>
                         </Card.Body>
+                        <p><FaCalendarAlt></FaCalendarAlt> {moment().add(10, 'days').calendar()}</p>
                     </Card>
                 </Col>
                 <Col>
-                    <Card>
-                        <Card.Img variant="top" src={third} />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                    <Card className='p-4 w-100 h-100'>
+                        <Card.Img className='image-fluid h-100' variant="top" src='https://media.fashionnetwork.com/m/e9eb/e93d/666d/0a7e/b387/c2f2/8e9f/0020/5cf9/8800/8800.jpg' />
+                        <Card.Body className='px-0'>
+                            <Card.Title>Fashion News</Card.Title>
                             <Card.Text>
-                                This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit
-                                longer.
+                            21 The Most Stylish Wedding Guest Dresses For Spring
                             </Card.Text>
                         </Card.Body>
+                        <p><FaCalendarAlt></FaCalendarAlt> {moment().add(10, 'days').calendar()}</p>
                     </Card>
                 </Col>
             </Row>
--- src/Pages/News/News/News.jsx
+++ src/Pages/News/News/News.jsx
@@ -3,20 +3,22 @@ import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import EditorsInside from '../EditorsInside/EditorsInside';
+import './News.css'
+import { FaArrowLeft } from "react-icons/fa";
 
 const News = () => {
     const news = useLoaderData()
     const {_id, title, image_url, details, category_id} = news
     return (
         <div>
-            <Card className='mb-5'>
+            <Card className='mb-5' style={{padding: '30px'}}>
                 <Card.Img variant="top" src={image_url} />
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
+                <Card.Body className='p-0 mt-4'>
+                    <Card.Title className='text-bold fs-3 mb-4'>{title}</Card.Title>
                     <Card.Text>
                        {details}
                     </Card.Text>
-                    <Link to={`/category/${category_id}`}><Button variant="primary">All news in this category</Button></Link>
+                    <Link to={`/category/${category_id}`}><Button className='btn-col'><FaArrowLeft></FaArrowLeft><span className='ms-3'>All news in this category</span></Button></Link>
                 </Card.Body>
             </Card>
             <EditorsInside></EditorsInside>
